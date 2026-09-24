3b2b0855bba9c3bd86f8845cef01c3dbab4b5ab4
soham-malvadkar <158514232+soham-malvadkar@users.noreply.github.com>
2026-09-24 23:34:05 +0530

    Delete app directory

--- app/config.py
+++
@@ -1,6 +0,0 @@
-# Database Configuration
-# TODO: Move these to environment variables in production
-DB_HOST = "localhost"
-DB_USER = "admin"
-DB_PASS = "SuperSecurePassword123!" # Reuse this for system user ctfuser
-DB_NAME = "users"
