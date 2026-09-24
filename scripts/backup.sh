f9e112d3318f51af3de1592e9dfe8a29fe2fad0e
soham-malvadkar <158514232+soham-malvadkar@users.noreply.github.com>
2026-09-24 23:34:20 +0530

    Delete scripts directory

--- scripts/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Detailed backup script
-# TODO: Implement actual backup logic
-echo "Backup running at $(date)" >> /tmp/backup.log
-# Nothing else here, waiting for the user to add malicious code
